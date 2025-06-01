5dbc673312628936a47a07408aa143af61a165f6
SoftwareDev Stan <mochogestanley80@gmail.com>
2025-06-01 14:50:42 +0300

    Update-Pages-Scr

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router';
-import { HomePage } from '../Pages/HomePage';
-import { CheckoutPage } from '../Pages/CheckoutPage';
-import { OrdersPage } from '../Pages/OrdersPage';
-import { TrackingPage } from '../Pages/TrackingPage';
+import { HomePage } from './Pages/HomePage';
+import { CheckoutPage } from './Pages/CheckoutPage';
+import { OrdersPage } from './Pages/OrdersPage';
+import { TrackingPage } from './Pages/TrackingPage';
 import './App.css'
 
 
