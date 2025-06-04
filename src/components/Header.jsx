dc422c9c72dc29a0d578555f4adbdb4ec89ed522
SoftwareDev Stan <mochogestanley80@gmail.com>
2025-06-04 14:01:13 +0300

    UpdatedHeaders-Page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,21 +7,17 @@ export function Header({ cart }){
         totalQuantity += cartItem.quantity
     });
 
-
     return(
         <div className="header">
             <div className="left-section">
                 <Link to="/" className="header-link">
-                <img className="logo"
-                    src="images/logo-white.png" />
-                <img className="mobile-logo"
-                    src="images/mobile-logo-white.png" />
+                <img className="logo" src="images/logo-white.png" />
+                <img className="mobile-logo" src="images/mobile-logo-white.png" />
                 </Link>
             </div>
 
             <div className="middle-section">
                 <input className="search-bar" type="text" placeholder="Search" />
-
                 <button className="search-button">
                 <img className="search-icon" src="images/icons/search-icon.png" />
                 </button>
@@ -29,7 +25,6 @@ export function Header({ cart }){
 
             <div className="right-section">
                 <Link className="orders-link header-link" to="/orders">
-
                 <span className="orders-text">Orders</span>
                 </Link>
 
@@ -39,6 +34,6 @@ export function Header({ cart }){
                 <div className="cart-text">Cart</div>
                 </Link>
             </div>
-            </div>
+        </div>
     );
 }
