05fc0e7dc5f75db8b67030d08ac1fca58e70bf24
SoftwareDev Stan <mochogestanley80@gmail.com>
2025-06-09 15:19:44 +0300

    Create Update/Delivery-Option-Feature

--- src/Pages/CheckoutPage.jsx
+++ src/Pages/CheckoutPage.jsx
@@ -7,7 +7,7 @@ import { PaymentSummary } from './PaymentSummary';
 import { CheckoutHeader } from './CheckoutHeader';
 
 
-export function CheckoutPage({ cart }){
+export function CheckoutPage({ cart, loadCart }){
     const [deliveryOptions, setDeliveryOptions] = useState([]);
     const [paymentSummary, setPaymentSummary] = useState(null);
 
@@ -19,8 +19,9 @@ export function CheckoutPage({ cart }){
             response = await axios.get('/api/payment-summary')
             setPaymentSummary(response.data);
         };
+
         fetchCheckoutData()
-    }, []);
+    }, [cart]);
 
     return (
         <>
@@ -30,7 +31,7 @@ export function CheckoutPage({ cart }){
             <div className="page-title">Review your order </div>
 
             <div className="checkout-grid">
-                <OrderSummary cart={cart} deliveryOptions={deliveryOptions}/>
+                <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart={loadCart}/>
                 <PaymentSummary paymentSummary={paymentSummary}/>
             </div>
             </div>
