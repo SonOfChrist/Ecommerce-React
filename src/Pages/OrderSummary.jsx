f4cc3959ac1870178ec39c228c08db472c3d7013
SoftwareDev Stan <mochogestanley80@gmail.com>
2025-07-03 08:08:17 +0300

    Update OrderSummary

--- src/Pages/OrderSummary.jsx
+++ src/Pages/OrderSummary.jsx
@@ -13,6 +13,12 @@ export function OrderSummary( {cart, deliveryOptions, loadCart }) {
 
                 });
 
+                const updateCartItem = async () => {
+                    await axios.put(`/api/cart-items/${cartItem.productId}`);
+                    await loadCart();
+                    console.log(updateCartItem)
+                };
+
                 const deleteCartItem = async () => {
                     await axios.delete(`/api/cart-items/${cartItem.productId}`);
                     await loadCart();
@@ -26,35 +32,22 @@ export function OrderSummary( {cart, deliveryOptions, loadCart }) {
                         </div>
 
                         <div className="cart-item-details-grid">
-                        <img className="product-image"
-                            src= {cartItem.product.image}/>
-
-                        <div className="cart-item-details">
-                            <div className="product-name">
-                            {cartItem.product.name}
+                            <img className="product-image" src= {cartItem.product.image}/>
+                            <div className="cart-item-details">
+                                <div className="product-name"> {cartItem.product.name} </div>
+                                <div className="product-price"> {formatMoney(cartItem.product.priceCents)} </div>
+                                <div className="product-quantity">
+                                    <span> Quantity: <span className="quantity-label">{cartItem.quantity}</span></span>
+                                    <span className="update-quantity-link link-primary" onClick={updateCartItem}> Update </span>
+                                    <span className="delete-quantity-link link-primary" onClick={deleteCartItem}> Delete </span>
+                                </div>
                             </div>
-                            <div className="product-price">
-                            {formatMoney(cartItem.product.priceCents)}
-                            </div>
-                            <div className="product-quantity">
-                            <span>
-                                Quantity: <span className="quantity-label">{cartItem.quantity}</span>
-                            </span>
-                            <span className="update-quantity-link link-primary">
-                                Update
-                            </span>
-                            <span className="delete-quantity-link link-primary" onClick={deleteCartItem}>
-                                Delete
-                            </span>
-                            </div>
-                        </div>
-
-                        <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart}/>
+                            <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart}/>
                         </div>
                     </div>
                 </>
                 )
-            })}
+            })};
         </div>
     );
 }
